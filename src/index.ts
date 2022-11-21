8b594b3cc877fbea81af0630fd1798b9787330be
lukeverso <cristhovamlucas@gmail.com>
2022-11-21 14:27:00 -0300

    Último commit (Adição do dotenv para proteger dados sensíveis)

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 import { router } from './router';
+import dotenv from 'dotenv';
 
-mongoose.connect('mongodb+srv://lukebase:Z3r0tr3z3@lukebase.bo164c5.mongodb.net/?retryWrites=true&w=majority')
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@lukebase.bo164c5.mongodb.net/?retryWrites=true&w=majority`)
      .then(() => {
           console.log('Conectado ao MongoDB Atlas.');
-
-          const app = express();
-
+          app.use((req, res, next) => {
+               res.header("Access-Control-Allow-Origin", "*");
+               res.header("Access-Control-Allow-Methods", "*");
+               res.header("Access-Control-Allow-Headers", "*");
+               next();
+          });
           app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-
           app.use(express.json());
-
           app.use(router);
-
-          app.listen(7000, () => {
-               console.log('Servidor rodando na porta http://localhost:7000.')
+          server.listen(7000, () => {
+               console.log('Servidor rodando na porta http://localhost:7000.');
           });
      })
      .catch((err) => {
